145e16e55ed8f67fbfe9f972ced8d514b13a904f
Eric Morse <morsee@rose-hulman.edu>
2020-09-14 20:43:09 +0000

    fixed up togglegpio.py

--- hw02/togglegpio.py
+++ hw02/togglegpio.py
@@ -9,6 +9,13 @@ import Adafruit_BBIO.GPIO as GPIO
 import gpiod
 import time
 import sys
+import signal
+
+#import C Shared library
+
+from ctypes import *
+so_file = "./gpio-utils.so"
+c_functions = CDLL(so_file)
 
 toggle = 0
 gpio = 60
@@ -17,15 +24,12 @@ SYSFS_GPIO_DIR = "/sys/class/gpio"
 MAX_BUF = 64
 ADC_BUF = 1024
 SYSFS_AIN_DIR = "/sys/devices/ocp.2/helper.11"
+keepgoing = 1
+def signal_handler(sig, frame):
+  print("Ctrl-C pressed, cleaning up and exiting..")
+  global keepgoing 
+  keepgoing = 0
 
-def gpio_export(gpio)
-
-  length = filename + str(len(filename)) + SYSFS_GPIO_DIR + "gpio" + str(gpio)
-  if(stat(filename, statBuffer) == 0): #It's already exported
-    print("gpio%d is already exported.")
-    return 0;
-  fd = open(SYSFS_GPIO_DIR + "/export", O_WRONLY)
-  return 1;
 print("Usage: " + sys.argv[0] + "<on/off time in us>")
 print("Toggle gpio 60 at the period given")
 
@@ -36,12 +40,22 @@ print("* ....blinking gpin 60         *")
 print("* ....period of " + str(2*onOffTime) + " us...........*")
 print("********************************")
 
+signal.signal(signal.SIGINT, signal_handler)
 #Using sysfs we need to write the gpio number to /sys/class
 #This will create the folder /sys/class/gpio/gpio60
-gpio_export(gpio)
+c_functions.gpio_export(gpio)
 
-printf("...export file accessed, new pin now accessible")
+print("...export file accessed, new pin now accessible")
 
 #Set Direction
-gpio_set_dir(gpio, "out")
-printf("...direction set to output")
+c_functions.gpio_set_dir(gpio, "out")
+print("...direction set to output")
+
+gpio_fd = c_functions.gpio_fd_open(gpio, 'O_RDONLY')
+
+while(keepgoing):
+  toggle = not toggle
+  c_functions.gpio_set_value(gpio, toggle)
+  time.sleep(onOffTime/1000000.0)
+
+c_functions.gpio_fd_close(gpio_fd)
